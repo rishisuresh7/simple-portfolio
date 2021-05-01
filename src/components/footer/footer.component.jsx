d7e73654cd2acf19b1b95e83ed84009f1c593eb5
Rishi Suresh <rishi9632@gmail.com>
2021-05-01 13:13:17 +0530

    master: update footer design

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -10,30 +10,41 @@ const Icon = (props) => {
 
 const Footer = () => {
     return (<div className="footer-container">
-        <div className="footer-option">
-            <Icon>
-                <i className="fa fa-envelope"></i>
-            </Icon>
-            <span>rishi9632@gmail.com</span>
-        </div>
-        <div className="footer-option">
-            <Icon>
-                <i className="fa fa-linkedin"></i>
-            </Icon>
-            <span>https://www.linkedin.com/in/rishi-suresh/</span>
-        </div>
-        <div className="footer-option">
-            <Icon>
-                <i className="fa fa-github"></i>
-            </Icon>
-            <span>https://github.com/rishisuresh7</span>
-        </div>
-        <div className="footer-option">
-            <Icon>
-                <i className="fa fa-instagram"></i>
-            </Icon>
-            <span>https://www.instagram.com/rishisuresh7</span>
-        </div>
+        <ul className='footer-content'>
+            <li className="footer-option">
+                <Icon>
+                    <i className="fa fa-envelope"></i>
+                </Icon>
+                <span>rishi9632@gmail.com</span>
+            </li>
+            <li className="footer-option">
+                <Icon>
+                    <i className="fa fa-linkedin"></i>
+                </Icon>
+                <span>https://www.linkedin.com/in/rishi-suresh</span>
+            </li>
+            <li className="footer-option">
+                <Icon>
+                    <i className='fa fa-mobile'></i>
+                </Icon>
+                <span>+91-8792615476</span>
+            </li>
+            <li className="footer-option">
+                <Icon>
+                    <i className="fa fa-github"></i>
+                </Icon>
+                <span>https://github.com/rishisuresh7</span>
+            </li>
+            <li className="footer-option">
+
+            </li>
+            <li className="footer-option">
+                <Icon>
+                    <i className="fa fa-instagram"></i>
+                </Icon>
+                <span>https://www.instagram.com/rishisuresh7</span>
+            </li>
+        </ul>
     </div>)
 }
 
